fb77a5f94bb0f68d68d1658f0361fa314f8dc209
urcop <kirillzama@list.ru>
2023-02-13 19:20:40 +0300

    updated the backend so that the reactor can take data from the local ip

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'api',
 ]
@@ -27,7 +28,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -91,3 +93,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL=True
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+]
--- frontend/src/components/reviews/mainReviews.jsx
+++ frontend/src/components/reviews/mainReviews.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './reviews.module.css'
+import axios from "axios";
+
 const MainReviews = () => {
+
+    const [reviews, setReviews] = useState()
+
+    async function fetchReviews() {
+        const response = await axios.get('http://localhost:8000/api/reviews/?format=json')
+        setReviews(response.data)
+    }
+
+    console.log(reviews)
+
     return (
         <div className={styles.reviews}>
-
+            <button onClick={fetchReviews}>GET REVIEWS</button>
         </div>
     );
 };
